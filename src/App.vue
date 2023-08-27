05a5ac61ccfc3b37a34cabbbb7193ad0601b4c32
Igor Xsky <igorxsky@gmail.com>
2023-08-27 17:09:10 +0300

    small refactor

--- src/App.vue
+++ src/App.vue
@@ -61,51 +61,14 @@
         Prev
       </button>
     </div>
-    <hr class="w-full border-t border-gray-600 my-4" />
-    
-    <section v-if="selectedCurrency" class="relative">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-        {{ selectedCurrency.name }} - USD
-      </h3>
-      <div 
-        class="flex items-end border-gray-600 border-b border-l h-64"
-        ref="graph"
-      >
-        <div 
-          v-for="(bar, idx) in normalizedGraph"
-          ref="graphElement"
-          :key="idx"
-          :style="{height: `${bar}%`, width: `${graphElementWidht}px`}"
-          class="bg-purple-800 border"
-        ></div>
-      </div>
-      <button
-        type="button"
-        class="absolute top-0 right-0"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.com/svgjs"
-          version="1.1"
-          width="30"
-          height="30"
-          x="0"
-          y="0"
-          viewBox="0 0 511.76 511.76"
-          style="enable-background:new 0 0 512 512"
-          xml:space="preserve"
-        >
-          <g>
-            <path
-              d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-              fill="#718096"
-              data-original="#000000"
-            ></path>
-          </g>
-        </svg>
-      </button>
-    </section>
+    <hr class="w-full border-t border-gray-600 my-4"/>
+    <graph-block
+      :graph="graph"
+      :mainCurrency="mainCurrency"
+      :selectCurrency="selectedCurrency"
+      @splice-graph="spliceGraph"
+      ref="graphBlock"
+    />
   </div>
 </div>
 </template>
@@ -114,6 +77,7 @@
 
 import { subscribeToCurrency, unsubscribeCurrency } from  './api'
 import AddCurrencyBlock from './components/AddCurrencyBlock.vue';
+import GraphBlock from './components/GraphBlock.vue';
 
 const PER_PAGE = 6;
 const EX_ROUND = 2;
@@ -121,17 +85,17 @@ const EX_ROUND = 2;
 export default {
   name: 'App',
   components: {
-    AddCurrencyBlock
+    'add-currency-block': AddCurrencyBlock,
+    'graph-block': GraphBlock
   },
   data() {
     return {
       currencies: [],
+      mainCurrency: 'USD',
       selectedCurrency: null,
       graph: [],
       page: 1,
       filter: '',
-      maxGraphElements: 1,
-      graphElementWidht: 38,
     }
   },
   created() {
@@ -154,12 +118,6 @@ export default {
       });
     }
   },
-  mounted() {
-    window.addEventListener("resize", this.calculateMaxGraphElements);
-  },
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphElements);
-  },
   computed: {
     startIndex() {
       return (this.page - 1) * PER_PAGE;
@@ -175,18 +133,6 @@ export default {
     },
     hasNextPage() {
       return this.filteredCurrencies.length > this.endIndex
-    },
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
     },
     stateOptions() {
       return {
@@ -213,27 +159,14 @@ export default {
     },
     selectedCurrency() {
       this.graph = [];
-      this.$nextTick(() => {
-        this.calculateMaxGraphElements();
-      });
     },
     currencies() {
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.currencies));
     }
   },
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return;
-      }
-      this.maxGraphElements = Math.round(this.$refs.graph.clientWidth / this.graphElementWidht);
-      this.updateGraphList();
-    },
-    updateGraphList() {
-      const graphLength = this.graph.length;
-      if (graphLength > this.maxGraphElements) {
-        this.graph.splice(0, graphLength - this.maxGraphElements);
-      }
+    spliceGraph(deleteCount) {
+      this.graph.splice(0, deleteCount);
     },
     updateCurrency(currencyName, price) {
       this.currencies
@@ -241,7 +174,6 @@ export default {
         .forEach(c => {
           if (c === this.selectedCurrency) {
             this.graph.push(c.price);
-            this.updateGraphList();
           }
           c.price = price;
         });
